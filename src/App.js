4fbe6e4bc1fe963db5f4fdfe46f0b36e4669a568
Leo <ing.victorerazo@hotmail.com>
2021-11-13 18:40:55 -0500

    barraOpciones

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BarraOpciones } from './components/BarraOpciones';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <BarraOpciones/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/components/BarraOpciones.js
+++ src/components/BarraOpciones.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 export const BarraOpciones = () => {
     return (
         <>
-            
-            <Navbar bg="primary" variant="dark">
+        <Container fluid>
+            <Navbar bg="primary" variant="dark" fixed="top">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#home">Pandaza2</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#features">Registro</Nav.Link>
+                        <Nav.Link href="#pricing">Tus recetas</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
-            
+        </Container>        
         </>
     )
 }
